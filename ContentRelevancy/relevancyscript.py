ec7ecfbcf1ac3e28b9f7d03d35d653170b52bb52
CalMcCormick <cal.mccormick@gmail.com>
2021-06-25 16:32:06 +0100

    Delete relevancyscript.py

--- ContentRelevancy/relevancyscript.py
+++
@@ -1,7 +0,0 @@
-# Created all the 
-
-from nlpscript import SiteKeywordDict
-
-link_to_url = "https://www.comparethemarket.com/home-insurance/"
-link_to = SiteKeywordDict(link_to_url)
-print(home_insurance.dictionary)
