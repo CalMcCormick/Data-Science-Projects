d153d34dccecd917efc6e22eb88aa03d0f880340
CalMcCormick <cal.mccormick@gmail.com>
2021-06-28 14:18:27 +0100

    Update mainscript.py

--- ContentRelevancy/mainscript.py
+++ ContentRelevancy/mainscript.py
@@ -2,11 +2,11 @@
 import pandas as pd
 from relevancycomparison import RelevancyScore
 
-home_insurance_links = pd.read_csv(r"c:\Users\calmc\OneDrive\Desktop\Portfolio Projects\Data Science Projects\ContentRelevancy\home_insurance_links.csv")
+insurance_links = pd.read_csv("mar-may-first-fifty.csv")
 
-links_from = home_insurance_links.LinkFrom
+links_from = insurance_links.LinkFrom
 
-links_to = home_insurance_links.LinkTo
+links_to = insurance_links.LinkTo
 
 
 relevency_figures = pd.DataFrame(columns=["LinkFrom", "LinkTo", "Relevency Percent", "Relevancy Score", "Relevancy Multiplier"])
@@ -20,16 +20,6 @@ for i in range(len(links_to)):
     relevency_figures = relevency_figures.append({"LinkFrom":links_from[i], "LinkTo":links_to[i], "Relevency Percent":percent, "Relevancy Score":score, "Relevancy Multiplier":multiplier}, ignore_index=True)
 
 
-relevency_figures.to_csv('home_insurance_relevancies_2.csv', index=False)
+relevency_figures.to_csv('mar-may-first-fifty-relevancies.csv', index=False)
 
-# print("\nLink 1:")
-# print(f"\nPercentage Keywords Hit: {link1.relevancy_percent}%")
-# print(f"Relevancy Score (Weighted): {link1.relevancy_score}")
-# print(f"Relevancy Multiplier: {link1.relevancy_multiplier}\n")
-
-
-# print("\nLink 2:")
-# print(f"\nPercentage Keywords Hit: {link2.relevancy_percent}%")
-# print(f"Relevancy Score (Weighted): {link2.relevancy_score}")
-# print(f"Relevancy Multiplier: {link2.relevancy_multiplier}\n")
 
