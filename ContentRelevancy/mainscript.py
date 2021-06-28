74f85075e8ce53d94993b60e5d21abf0173bd6bb
CalMcCormick <cal.mccormick@gmail.com>
2021-06-28 15:03:05 +0100

    Update mainscript.py

--- ContentRelevancy/mainscript.py
+++ ContentRelevancy/mainscript.py
@@ -1,16 +1,23 @@
 # This is where everything will be put together in the end
 import pandas as pd
+from tkinter import *
 from relevancycomparison import RelevancyScore
+from tkinter import filedialog
 
-insurance_links = pd.read_csv("mar-may-first-fifty.csv")
+# Allowing to just choose a file, rather than specifying the actual name
+filename = filedialog.askopenfilename(initialdir='/', title='Select CSV File')
+new_file_name = ".csv"
 
+# Reading the CSV file 
+insurance_links = pd.read_csv(filename)
+# Seperating the links from and links to
 links_from = insurance_links.LinkFrom
-
 links_to = insurance_links.LinkTo
 
-
+# Creating a dataframe template for the new CSV
 relevency_figures = pd.DataFrame(columns=["LinkFrom", "LinkTo", "Relevency Percent", "Relevancy Score", "Relevancy Multiplier"])
 
+# Looping through every URL and 
 for i in range(len(links_to)):
     relevancy = RelevancyScore(links_from[i], links_to[i])
     percent = relevancy.relevancy_percent
@@ -19,7 +26,6 @@ for i in range(len(links_to)):
     
     relevency_figures = relevency_figures.append({"LinkFrom":links_from[i], "LinkTo":links_to[i], "Relevency Percent":percent, "Relevancy Score":score, "Relevancy Multiplier":multiplier}, ignore_index=True)
 
-
-relevency_figures.to_csv('mar-may-first-fifty-relevancies.csv', index=False)
-
+# As I get better with GUI's I'll update this so you can select what you want to call it.
+relevency_figures.to_csv(new_file_name, index=False)
 
